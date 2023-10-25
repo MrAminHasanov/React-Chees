65d2c4c38fb18cb4dff0aa26be8af527e87a9586
Amin Hesenov <lolotron022gmail.com>
2023-10-25 18:19:17 +0400

    added knight visual

--- src/component/Body/Table/TableContent/Square/Figures/Knight/Knight.jsx
+++ src/component/Body/Table/TableContent/Square/Figures/Knight/Knight.jsx
@@ -2,8 +2,19 @@ import c from './Knight.module.scss'
 
 function Knight() {
     return (
-        <div className={c.component}>Knight</div>
-    )
+        <div className={c.component}>
+            <div className={c.head}>
+                <div className={c.topRounde}></div>
+                <div className={c.mouthe}></div>
+            </div>
+            <div className={c.body}>
+                <div className={c.backgrounde}></div>
+                <div className={c.rightRounde}></div>
+                <div className={c.leftRounde}></div>
+            </div>
+            <div className={c.bottomRounde}></div>
+            <div className={c.bottom}></div>
+        </div >)
 }
 
 export default Knight
