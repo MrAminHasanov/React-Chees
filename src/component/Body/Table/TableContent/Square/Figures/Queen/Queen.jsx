066dd047300efaed8137aa9b8f9e206208341be3
Amin Hesenov <lolotron022gmail.com>
2023-11-20 14:17:25 +0400

    created queen visual

--- src/component/Body/Table/TableContent/Square/Figures/Queen/Queen.jsx
+++ src/component/Body/Table/TableContent/Square/Figures/Queen/Queen.jsx
@@ -1,9 +1,21 @@
+import classNames from 'classnames';
 import c from './Queen.module.scss';
 
 function Queen() {
   return (
-    <div className={c.component}>Queen</div>
-  )
+    <div className={c.component}>
+      <div className={c.top}>
+      </div>
+      <div className={c.cap}></div>
+      <div className={c.topRounde}></div>
+      <div className={c.body}>
+        <div className={classNames(c.leftRounde, c.rounde)}></div>
+        <div className={c.midle}></div>
+        <div className={classNames(c.rightRounde, c.rounde)}></div>
+      </div>
+      <div className={c.bottomRounde}></div>
+      <div className={c.bottom}></div>
+    </div >)
 }
 
 export default Queen
