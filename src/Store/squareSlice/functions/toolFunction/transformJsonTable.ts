5a9253e932c3bac012787d0134f6c37cb10cdfc9
Amin Hesenov <lolotron022gmail.com>
2024-01-20 14:54:18 +0400

    optimized transformJson function

--- src/Store/squareSlice/functions/toolFunction/transformJsonTable.ts
+++ src/Store/squareSlice/functions/toolFunction/transformJsonTable.ts
@@ -1,22 +1,17 @@
 import { contentInter } from './../../Types/stateInterface';
 import { figures } from "../../Types/constFigureNames.ts"
 
-const transformJsonToTableContent = (json: { [figureName: string]: Array<number> }) => {
-    const content: contentInter = new Array(64);
-
-    Object.keys(figures).forEach((figureKey: string): void => {
-        if (figureKey !== "emptySquare") {
-            json[figureKey].forEach((squareId: number): void => {
-                content[squareId] = figures[figureKey]
-            })
-        } else {
-            for (let id: number = 0; id < 64; ++id) {
-                if (!(id in content)) {
-                    content[id] = figures.emptySquare
-                }
-            }
-        }
-    })
+const transformJsonToTableContent = (contentList: { [figureName: string]: Array<number> }) => {
+    const content: contentInter = [];
+    for (let i = 0; i <= 63; ++i) {
+        content.push(figures.emptySquare);
+    }
+    
+    Object.entries(contentList).forEach(([figureName, figurePosArr]) => {
+        figurePosArr.forEach((figurePos) => {
+            content[figurePos] = figures[figureName];
+        })
+    });
 
     return (content)
 }
